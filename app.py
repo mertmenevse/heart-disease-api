28b5f19a6dccab0d1a444f74f825a52175c613d8
Mehmet Mert MENEVŞE <mert.menevse32@gmail.com>
2025-04-06 15:51:23 +0300

    Initial commit with CI/CD

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('heart_disease_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # JSON verisini al
+    data = request.get_json()
+    
+    # Özellikleri numpy dizisine çevir
+    features = np.array(data['features']).reshape(1, -1)
+    
+    # Tahmin yap
+    prediction = model.predict(features)
+    
+    # Sonucu JSON olarak döndür
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
